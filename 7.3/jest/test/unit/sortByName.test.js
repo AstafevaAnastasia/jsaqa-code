182bd84fe911eb9d6993d49af89eebe06acb6443
AstafevaAnastasia <privet200426@gmail.com>
2026-01-02 23:41:51 +0400

    fix

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -1,17 +1,25 @@
-const sorting = require("../../app");
+const { sortByName } = require('../../app');
 
-describe("Books names test suit", () => {
-  it("Books names should be sorted in ascending order", () => {
+describe('Books names test suit', () => {
+  it('Books names should be sorted in ascending order', () => {
     expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
+      sortByName([
+        'Гарри Поттер',
+        'Властелин Колец',
+        'Волшебник изумрудного города',
       ])
     ).toEqual([
-      "Властелин Колец",
-      "Волшебник изумрудного города",
-      "Гарри Поттер",
+      'Властелин Колец',
+      'Волшебник изумрудного города',
+      'Гарри Поттер',
     ]);
+  });
+
+  it('Returns original array when all names are equal', () => {
+    const input = ['Азбука', 'Азбука'];
+    const result = sortByName(input);
+    expect(result).toEqual(['Азбука', 'Азбука']);
+
+    expect(result).toBe(input);
   });
 });
